2260f22878bc3ca25f21c71669e7518f9fc7ad96
alexandramandy <alexandra.mandy322@gmail.com>
2022-06-13 09:02:29 +0200

    First front commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Team from '../Team/Team';
+import Header from '../Header/Header';
+
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Team />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import logo from '../../assets/images/logo.png';
+import './Header.scss';
+
+function Header() {
+    return (
+        <div className="header">
+            <img src={logo} className="logo" alt="Logo My Little Garden" />
+            <h1 className="title">My Little Garden</h1>
+            <div className="buttonSection">
+                <button className="button">S'inscrire</button>
+                <button className="button">Se connecter</button>
+            </div>
+        </div>
+    );
+  }
+
+export default Header;
