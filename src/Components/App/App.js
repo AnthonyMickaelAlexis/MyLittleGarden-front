6bae510ddac04525a70fc2e5e83573089897aef8
BilelMat <matmouribilell@gmail.com>
2022-06-14 13:02:20 +0200

    page d'accueil moitie

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,15 +4,19 @@ import Header from '../Header/Header';
 import Inscription from '../Inscription/Inscription'
 import LoginPage from '../LoginPage/LoginPage';
 import Error from '../404/404';
+import HomePage from '../HomePage/HomePage';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="app">
+      <HomePage/>
       <Header />
       <Team />
       <Inscription />
       <LoginPage />
       <Error />
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Background from '../Background/Background';
+import Header from '../Header/Header';
+
+
+const HomePage = () => {
+    return (
+        <div>
+        <Header/>
+        <Background/>
+        </div>
+    )
+};
+
+export default HomePage;
