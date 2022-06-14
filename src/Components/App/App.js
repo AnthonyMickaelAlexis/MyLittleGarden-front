04da821c58497e60c82c1a7cb1f56d4b0fb4b1c7
BilelMat <matmouribilell@gmail.com>
2022-06-14 09:39:48 +0200

    header team inscription loginPage 404

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import Team from '../Team/Team';
 import Header from '../Header/Header';
-<<<<<<< HEAD
-import Inscription from '../Inscription/Inscription';
-
-=======
 import Inscription from '../Inscription/Inscription'
 import LoginPage from '../LoginPage/LoginPage';
 import Error from '../404/404';
->>>>>>> 06-13-Alexandra
 
 function App() {
   return (
