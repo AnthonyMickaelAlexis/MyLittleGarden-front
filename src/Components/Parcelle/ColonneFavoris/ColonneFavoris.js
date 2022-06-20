db4fe50962b6ff77fc93ad54c3a035211f6b419a
alexandramandy <alexandra.mandy322@gmail.com>
2022-06-20 13:26:56 +0200

    Buttun show/hide vegetable, search bar, vegetables list

--- src/Components/Parcelle/ColonneFavoris/ColonneFavoris.js
+++ src/Components/Parcelle/ColonneFavoris/ColonneFavoris.js
@@ -1,22 +1,38 @@
-import PropTypes from 'prop-types';
+import { useState } from 'react';
 import './ColonneFavoris.scss';
 
-function ColonneFavoris({ open, toggle }) {
+function ColonneFavoris() {
 
-    const cssClassNames = open ? 'toggler toggler--open' : 'toggler';
+    const [show, setShow] = useState(true)
 
     return (
-        <div className="colonneFav">
-        <button className={cssClassNames} type="button" onClick={toggle}>
-            Afficher/Cacher
-        </button>
+        <div className="favoris">
+            <button onClick={() => setShow(!show)} className="favButton"> 
+                Afficher/Masquer la liste des légumes
+            </button>
+            {
+                show?<div className="colonneFav">
+                    <h3 className="favTittle">Liste des légumes</h3>
+                    <input
+                        type="text"
+                        className="searchVegetable"
+                        placeholder="Rechercher un légume"
+                    />
+                    <ul className="vegetableList">
+                        <li className="vegetable">Tomates</li>
+                        <li className="vegetable">Courgettes</li>
+                        <li className="vegetable">Haricots verts</li>
+                    </ul>
+                </div>:null
+            }
         </div>
-    );
+    )
 };
 
-ColonneFavoris.propTypes = {
-    open: PropTypes.bool.isRequired,
-    toggle: PropTypes.func.isRequired,
-  };
-
 export default ColonneFavoris;
+
+
+// A faire : 
+//     - Faire fonctionner la recherche
+//     - Adapter le texte du bouton afficher/masquer
+//     - Rendre dynamique la liste des légumes (BDD)
--- src/Components/Parcelle/Parcelle.js
+++ src/Components/Parcelle/Parcelle.js
@@ -2,14 +2,15 @@ import Grille from "../Parcelle/Grille/Grille";
 import ColonneFavoris from "./ColonneFavoris/ColonneFavoris";
 import "./Parcelle.scss";
 
+
 function Parcelle() {
 
-    return (
+      return (
         <div className="ParcellePage">
             <ColonneFavoris />
             <Grille />
         </div>
-    );
-};
-
-export default Parcelle;
+      );
+    };
+  
+  export default Parcelle;
