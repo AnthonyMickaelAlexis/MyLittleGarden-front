159ec2cd3ed4b3bcf22353b902ddb59d43638f57
alexandramandy <alexandra.mandy322@gmail.com>
2022-06-27 23:41:26 +0200

    CSS improvement (design, responsivity, logo link to home page)

--- src/Components/Parcelle/Grille/Grille.js
+++ src/Components/Parcelle/Grille/Grille.js
@@ -9,18 +9,41 @@ const verticalSquaresWanted = Array(5).fill(".");
 
 // A faire par la suite : laisser le choix de la taille à l'utilisateur
 
-function Grille({cropsToParcel, setCropsToParcel, isCropSelected, setIsCropSelected}) {
+function Grille({
+  cropsToParcel,
+  setCropsToParcel,
+  isCropSelected,
+  setIsCropSelected,
+}) {
   return (
     <div className="container">
-    <h1 className="parcelTitle">Ma parcelle</h1>
-    <div className="parcelBoard">
-      {
-        // x = index
-        verticalSquaresWanted.map((boite, y) => {
-          return <Row  cropsToParcel={cropsToParcel} setCropsToParcel={setCropsToParcel} y={y} isCropSelected={isCropSelected} setIsCropSelected={setIsCropSelected} />;
-        })
-      }
-    </div>
+      <h1 className="parcelTitle">Ma parcelle</h1>
+      <div className="explanationSection">
+      <p className="explanation"><span className="step">Etape 1 :</span>
+         cliquez sur l'image de votre légume favoris
+      </p>
+      <p className="explanation"><span className="step">
+        Etape 2 :</span> cliquez sur le morceau de parcelle de votre choix
+      </p>
+      <p className="explanation"><span className="step">Etape 3 :</span> Recommencez</p>
+</div>
+      <div className="parcelBoard">
+        {
+          // x = index
+          verticalSquaresWanted.map((boite, y) => {
+            return (
+              <Row
+                cropsToParcel={cropsToParcel}
+                setCropsToParcel={setCropsToParcel}
+                y={y}
+                isCropSelected={isCropSelected}
+                setIsCropSelected={setIsCropSelected}
+              />
+            );
+          })
+        }
+      <button type="button" className="saveParcel">Sauvegarder ma parcelle</button>
+      </div>
     </div>
   );
 }
