7e07b4d7932604b3919254d3101ec2543ebcc94c
PierreMendes1 <pemendes06@gmail.com>
2022-06-28 16:16:33 +0200

    validation

--- src/Components/Validation/validation.js
+++ src/Components/Validation/validation.js
@@ -1,13 +1,13 @@
 
-export default function Validation(user_name, password, email, fristname, lastname) {
+export default function Validation(user_name, password, email, firstname, lastname) {
 
   let errors={};
 
   if(!user_name){
     errors.user_name = "Votre Nom d'utilisateur est nécessaire."
   }
-  if(!fristname){
-    errors.fristname = "Votre Prenom est nécessaire."
+  if(!firstname){
+    errors.firstname = "Votre Prenom est nécessaire."
   }
   if(!lastname){
     errors.lastname = "Votre Nom est nécessaire."
@@ -15,11 +15,11 @@ export default function Validation(user_name, password, email, fristname, lastna
   if(!email){
     errors.email = "Votre email est nécessaire."
   }else if(!/\S+@\.\S+/.test(email)){
-    errors.email= "Votre email est invalide"
+    errors.emailNotValid= "Votre email est invalide"
   }if(!password){
     errors.password = "Votre mot de passe est nécessaire."
   }else if(password.length < 3){
-    errors.password = "Votre mot de passe doit contenir au moins 3 caractères"
+    errors.passwordLength = "Votre mot de passe doit contenir au moins 3 caractères"
   }
 
 
